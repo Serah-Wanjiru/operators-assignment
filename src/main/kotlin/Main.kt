a8dddb6bcdf9b487eedcbdcbdc23d531199e265b
serah wanjiru <WanjiruMburuuu@gmail.com>
2023-02-09 08:00:34 +0300

    operators

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main() {
+name()
+sum(76,56,89,98)
+add()
+fact()
+}
+fun name (){
+    var name = "serah"
+    println("hello" + " " + name)
+}
+fun add(){
+    var z = 40
+    var v = 15
+    var sum = z % v
+    println(sum)
+}
+
+
+
+fun sum (num1:Int, num2:Int, num3:Int,  num4:Int){
+    var sum= num1 + num2 + num3 + num4
+    println(sum)
+}
+fun fact(){
+    var fact= ("am capable and i love reading")
+    println(fact)
+}
+
+
+
+
+
+
+
+
+
+
+
